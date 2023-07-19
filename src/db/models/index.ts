9d003f5bd347487ffad456734dd108b5ca1c6ec2
Richie Permana <richie.permana@gmail.com>
2023-07-19 23:22:18 +0800

    feat: Added Orders Model and Migration

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -2,5 +2,6 @@ export * from './airports.entity';
 export * from './car-class.entity';
 export * from './currencies.entity';
 export * from './guests.entity';
+export * from './orders.entity';
 export * from './price-schema.entity';
 export * from './users.entity';
