03f871fde5dc38af08bffd9f39aaf37bda344060
Richie Permana <richie.permana@gmail.com>
2023-07-24 00:56:13 +0800

    feat: Added CreateAirportDto

--- src/module/airports/dto/create-airport.dto.ts
+++ src/module/airports/dto/create-airport.dto.ts
@@ -1,1 +1,19 @@
-export class CreateAirportDto {}
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAirportDto {
+  @ApiPropertyOptional()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiPropertyOptional()
+  @IsNotEmpty()
+  @IsString()
+  latitude: string;
+
+  @ApiPropertyOptional()
+  @IsNotEmpty()
+  @IsString()
+  longitude: string;
+}
