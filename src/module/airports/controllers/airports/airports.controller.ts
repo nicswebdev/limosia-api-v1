7bdafd0796e2d6c8e2eaa5bd89d31d204f5eca27
Richie Permana <richie.permana@gmail.com>
2023-07-24 00:38:00 +0800

    feat: Added findAll Endpoint for Airports

--- src/module/airports/controllers/airports/airports.controller.ts
+++ src/module/airports/controllers/airports/airports.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { ApiPaginatedResponse, ApiSingleResponse } from '@/common/decorators';
+import { PaginationQuery, PaginatedDto } from '@/common/dto';
+import { Airports } from '@/db/models';
+import { Controller, Get, Param, Query, UseFilters } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { IPaginationOptions } from 'nestjs-typeorm-paginate';
+import { AirportsService } from '../../services/airports/airports.service';
+import { QueryNotFoundFilter } from '@/common/filters';
 
 @Controller('airports')
-export class AirportsController {}
+export class AirportsController {
+  constructor(private readonly airportService: AirportsService) {}
+
+  @Get()
+  @ApiPaginatedResponse({
+    model: Airports,
+    apiOkDescription: 'The records has been successfully returned.',
+    summary: 'Find all of existing Airports.',
+  })
+  findAll(
+    @Query() paginationQuery: PaginationQuery,
+  ): Promise<PaginatedDto<Airports>> {
+    const { page, limit, search, sortBy } = paginationQuery;
+    const options: IPaginationOptions & PaginationQuery = {
+      page,
+      limit,
+      search,
+      sortBy,
+    };
+
+    return this.airportService.findAllPaginate({ ...options });
+  }
+
+}
--- src/module/airports/services/airports/airports.service.ts
+++ src/module/airports/services/airports/airports.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { PaginationQuery, PaginatedDto } from '@/common/dto';
+import { Airports } from '@/db/models';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IPaginationOptions, paginate } from 'nestjs-typeorm-paginate';
+import { Repository, Like } from 'typeorm';
 
 @Injectable()
-export class AirportsService {}
+export class AirportsService {
+  constructor(
+    @InjectRepository(Airports)
+    private readonly airportRepository: Repository<Airports>,
+  ) {}
+
+  async findAllPaginate(
+    options: IPaginationOptions & PaginationQuery,
+  ): Promise<PaginatedDto<Airports>> {
+    const val = await paginate<Airports>(
+      this.airportRepository,
+      { ...options },
+      {
+        where: {
+          name: options.search && Like(`%${options.search}%`),
+        },
+        order: {
+          id: options.sortBy,
+        },
+      },
+    );
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Success',
+
+      ...val,
+    };
+  }
+
+}
