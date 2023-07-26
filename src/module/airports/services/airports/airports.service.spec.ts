a8979779ec17a1803e5a350b83115d0d8b1c94c1
Richie Permana <richie.permana@gmail.com>
2023-07-26 14:26:05 +0800

    test: Update Unit Test

--- src/module/airports/services/airports/airports.service.spec.ts
+++ src/module/airports/services/airports/airports.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AirportsService } from './airports.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Airports } from '@/db/models';
+import { Repository } from 'typeorm';
 
 describe('AirportsService', () => {
   let service: AirportsService;
+  let airportRepository: Repository<Airports>;
+
+  const AIRPORT_REPOSITORY_TOKEN = getRepositoryToken(Airports);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AirportsService],
+      providers: [
+        AirportsService,
+        {
+          provide: AIRPORT_REPOSITORY_TOKEN,
+          useValue: {
+            findAllPaginate: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            findOne: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AirportsService>(AirportsService);
+    airportRepository = module.get<Repository<Airports>>(
+      AIRPORT_REPOSITORY_TOKEN,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('airportRepository should be defined', () => {
+    expect(airportRepository).toBeDefined();
   });
 });
