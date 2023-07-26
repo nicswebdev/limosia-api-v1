04feecbe11eb85891a67c64a72ada9479b68ad96
Richie Permana <richie.permana@gmail.com>
2023-07-26 14:33:39 +0800

    feat: Config DataSource Options for Seeders

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,17 +1,23 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 import * as dotenv from 'dotenv';
+import { SeederOptions } from 'typeorm-extension';
+import { MainSeeder } from './seeds/main.seeder';
+
 dotenv.config();
 
-export const dataSourceOptions: DataSourceOptions = {
+export const dataSourceOptions: DataSourceOptions & SeederOptions = {
   type: 'mysql',
   host: 'localhost',
   port: 3306,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: ['dist/**/*.entity.js'],
+  entities: ['dist/**/*.entity.js', `${__dirname}/**/*.entity.{ts,js}`],
   migrations: ['dist/db/migrations/*.js'],
+  // seeds: ['src/db/seeds/**/*{.ts, .js}'],
+  // factories: ['src/db/factories/**/*{.ts, .js}'],
+  seeds: [MainSeeder],
   synchronize: false,
 };
 
