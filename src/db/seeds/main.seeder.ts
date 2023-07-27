3a9296c504a947b4194191ba0080b189b040877a
Richie Permana <richie.permana@gmail.com>
2023-07-27 13:55:26 +0800

    refactor: Delete Unused Imports

--- src/db/seeds/main.seeder.ts
+++ src/db/seeds/main.seeder.ts
@@ -1,6 +1,5 @@
 import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager, runSeeders } from 'typeorm-extension';
-import { dataSourceOptions } from '../data-source';
 import CreateRoleSeeder from './create-roles.seeder';
 
 export class MainSeeder implements Seeder {
