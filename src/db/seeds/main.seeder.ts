09d20d4221ec4b76557f922566f149f7b0924f0b
Richie Permana <richie.permana@gmail.com>
2023-08-13 01:25:13 +0800

    feat: Added SuperAdmin User Seeder

--- src/db/seeds/main.seeder.ts
+++ src/db/seeds/main.seeder.ts
@@ -1,6 +1,7 @@
 import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager, runSeeders } from 'typeorm-extension';
 import CreateRoleSeeder from './create-roles.seeder';
+import CreateUsersAdminSeeder from './create-users-admin.seeder';
 
 export class MainSeeder implements Seeder {
   async run(
@@ -8,7 +9,7 @@ export class MainSeeder implements Seeder {
     factoryManager: SeederFactoryManager,
   ): Promise<any> {
     await runSeeders(dataSource, {
-      seeds: [CreateRoleSeeder],
+      seeds: [CreateRoleSeeder, CreateUsersAdminSeeder],
     });
   }
 }
