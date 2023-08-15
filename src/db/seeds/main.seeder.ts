c20d5fdd70df0cbf6ada41412da455f68394419d
Richie Permana <richie.permana@gmail.com>
2023-08-15 23:20:44 +0800

    feat: Added Dummy Order Seeders

--- src/db/seeds/main.seeder.ts
+++ src/db/seeds/main.seeder.ts
@@ -2,6 +2,7 @@ import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager, runSeeders } from 'typeorm-extension';
 import CreateRoleSeeder from './create-roles.seeder';
 import CreateUsersAdminSeeder from './create-users-admin.seeder';
+import CreateOrdersSeeder from './create-orders.seeder';
 
 export class MainSeeder implements Seeder {
   async run(
@@ -9,7 +10,7 @@ export class MainSeeder implements Seeder {
     factoryManager: SeederFactoryManager,
   ): Promise<any> {
     await runSeeders(dataSource, {
-      seeds: [CreateRoleSeeder, CreateUsersAdminSeeder],
+      seeds: [CreateRoleSeeder, CreateUsersAdminSeeder, CreateOrdersSeeder],
     });
   }
 }
