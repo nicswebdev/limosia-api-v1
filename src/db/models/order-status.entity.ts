2de3d72aaa801c29acd107d3c04d92c05b8526e8
Richie Permana <richie.permana@gmail.com>
2023-08-17 03:22:46 +0800

    feat: Added New Models Entity

---
+++ src/db/models/order-status.entity.ts
@@ -0,0 +1,47 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Order } from './orders.entity';
+import { Exclude, Expose } from 'class-transformer';
+import { IsBoolean } from 'class-validator';
+
+@Entity('order_statuses')
+@Exclude()
+export class OrderStatus {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column({ unique: true })
+  @Expose()
+  name: string;
+
+  @ApiProperty()
+  @Column({ nullable: true })
+  @Expose()
+  description: string;
+
+  @ApiProperty()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @ApiProperty()
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @ApiProperty()
+  @Column({ default: false })
+  @IsBoolean()
+  delete_row: boolean;
+
+  @ApiProperty()
+  @OneToOne(() => Order, (order) => order.order_status)
+  order: Order | number;
+}
